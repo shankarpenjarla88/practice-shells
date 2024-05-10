95c85739816060d227573274799f322bbbb1736c
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-10 03:38:53 -0700

    userandpasswd

--- file1.sh
+++ file1.sh
@@ -1,11 +1,17 @@
 #!/bin/bash ---> it is sheband to that we are informing linux to execute the script.
 
-rtr1=$1
-rtr2=$2
 
-echo "$1 :we can use it as version control,review the changes,security and backup,collobaration"
-echo "$2 : is automatio to linux and we deploy our project using $2"
+echo "Enter your username"
 
+read username
+
+echo "Entering my username is : $username /n"
+
+echo "Enter your password"
+
+read password
+
+echo "Entering my password is : $password"
 
 
 
